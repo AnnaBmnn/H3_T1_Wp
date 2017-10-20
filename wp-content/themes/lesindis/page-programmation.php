da0e7cb57f7d5e7b249c1790db9bb8b64144e845
AnnaBmnn <annabaumann@laposte.net>
2017-10-20 15:50:28 +0200

    :lipstick: integration page programmation

--- wp-content/themes/lesindis/page-programmation.php
+++ wp-content/themes/lesindis/page-programmation.php
@@ -1,9 +1,9 @@
 <?php
 get_header(); //appel du template header.php  ?>
 
-<div id="content">
+<div id="content" class="content-programmation">
     <div class="container">
-        <section class="section section-top-programmation">
+        <section class="section section-top-progra">
             <div class="section__title--big">
                 <div class="section__title--left">
                     <span>Au</span>
@@ -11,48 +11,146 @@ get_header(); //appel du template header.php  ?>
                 </div>
                 <div class="section__title--right">
                     <span>gramme</span>
+                    <div class="section__date--big">
+                        Du 2 au 13 Nov.
+                    </div>
                 </div>
             </div>
-            <div class="section__date--big">
-                Du 2 au 13 Nov.
-            </div>
+
         </section>
         <section class="section">
             <h2 class="section__sur-title">
                 Artistes
             </h2>
-            <div class="section__liste">
+            <div class="section__liste-artiste">
                 <ul>
-                    <li class="test-1 section__liste__item">Mon super nom d'articles</li>
-                    <li class="test-2 section__liste__item">Mon  nom d'articles</li>
-                    <li class="test-3 section__liste__item">Mon super nom d'articles</li>
-                    <li class="test-4 section__liste__item"> super nom d'articles</li>
-                    <li class="section__liste__item">Mon super  d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item">Mon  nom d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item"> super nom d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item">Mon  nom d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item">Mon  nom d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item">Mon  nom d'articles</li>
-                    <li class="section__liste__item">Mon super nom d'articles</li>
-                    <li class="section__liste__item"> super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon  nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem"> super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super  d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon  nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem"> super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon  nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon  nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon  nom d'articles</li>
+                    <li class="section__liste-artisteItem">Mon super nom d'articles</li>
+                    <li class="section__liste-artisteItem"> super nom d'articles</li>
                 </ul>
             </div>
         </section>
-        <section class="section">
+        <!-- SECTION DATE -->
+        <section class="section section-date">
             <h2 class="section__sur-title">
                 Date
             </h2>
-
+            <!-- WARM UP -->
+            <div class="section-date__partie section-date__partie--left">
+                <div class="section__title--medium">
+                    Warm up
+                </div>
+                <div class="section__date--medium">2 & 4 Oct.</div>
+                <div class="artiste--left artiste">
+                    <div class="artiste__name">Malik Djoui</div>
+                    <div class="artiste__infos">
+                        <div class="artiste__lieu">Le manège</div>
+                        <div class="artiste__adresse">13 rue de la pompe à Lorient</div>
+                    </div>
+                    <div class="artiste__date">Mar. 3 Oct.</div>
+                </div>
+                <div class="artiste--left artiste">
+                    <div class="artiste__name">Swans + Le secret DJ set</div>
+                    <div class="artiste__infos">
+                        <div class="artiste__lieu">Le manège</div>
+                        <div class="artiste__adresse">13 rue de la pompe à Lorient</div>
+                    </div>
+                    <div class="artiste__date">Mer. 4 Ooct</div>
+                </div>
+            </div>
+            <!-- CONCERT -->
+            <div class="section-date__partie--right section-date__partie">
+                <div class="section__title--medium">
+                    Concert
+                </div>
+                <div class="section__date--medium">Du 4 au 12 nov.</div>
+            </div>
+            <div class="agenda">
+                <ul>
+                    <li class="agenda__day">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                    <li class="agenda__day">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                    <li class="agenda__day">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                    <li class="agenda__day agenda__day--active">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                    <li class="agenda__day">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                    <li class="agenda__day">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                    <li class="agenda__day">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                    <li class="agenda__day">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                    <li class="agenda__day">
+                        <div class="agenda__dayText">Mar.</div>
+                        <div class="agenda__dayNumber">9</div>
+                    </li>
+                </ul>
+            </div>
+            <div class="section-date__partie--right section-date__partie">   
+                <div class="artiste--right artiste">
+                    <div class="artiste__name">Malik Djoui</div>
+                    <div class="artiste__infos">
+                        <div class="artiste__lieu">Le manège</div>
+                        <div class="artiste__adresse">13 rue de la pompe à Lorient</div>
+                    </div>
+                    <div class="artiste__date">Mar. 3 Oct.</div>
+                </div>
+                <div class="artiste--right artiste">
+                    <div class="artiste__name">Swans + Le secret DJ set</div>
+                    <div class="artiste__infos">
+                        <div class="artiste__lieu">Le manège</div>
+                        <div class="artiste__adresse">13 rue de la pompe à Lorient</div>
+                    </div>
+                    <div class="artiste__date">Mer. 4 Oct</div>
+                </div>
+            </div>
+            <div class="section-date__partie--right section-date__partie">
+                <div class="artiste__picture">
+                    <img src="https://img.20mn.fr/4XYkpJZNQyCzRK4bGFs_6A/1200x768_chat-illustration" alt="">
+                </div>
+            </div>
+            <div class="clear-both"></div>
+            
         </section>
-        <section class="section">
+        <div class="clear-both"></div>
+
+        <section class="section section-lieu">
             <h2 class="section__sur-title">
                 Lieu
             </h2>
